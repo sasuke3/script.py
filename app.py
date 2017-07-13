3c2b8f3c25db696dfc40bccf2da4729ddbfa8ae1
sasuke3 <stephendean69@yahoo.in>
2017-07-13 11:48:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,42 +1,80 @@
 #!/usr/bin/env python
 
-import urllib
+from __future__ import print_function
+from future.standard_library import install_aliases
+install_aliases()
+
+from urllib.parse import urlparse, urlencode
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
+
 import json
 import os
+import sys
+import logging
 
-from flask import Flask
+from flask import Flask, render_template
 from flask import request
 from flask import make_response
 
 # Flask app should start in global layout
 app = Flask(__name__)
-
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 
 @app.route('/webhook', methods=['POST'])
+
 def webhook():
     req = request.get_json(silent=True, force=True)
 
     print("Request:")
     print(json.dumps(req, indent=4))
 
-    res = makeWebhookResult(req)
+    res = processRequest(req)
 
     res = json.dumps(res, indent=4)
-    print(res)
+    # print(res)
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
     return r
 
-def makeWebhookResult(req):
-    if req.get("result").get("action") != "shipping.cost":
+def processRequest(req):
+    if req.get("result").get("action") != "bookMyConference":
         return {}
-    result = req.get("result")
-    parameters = result.get("parameters")
-    zone = parameters.get("shipping-zone")
 
-    cost = {'Europe':100, 'North America':200, 'South America':300, 'Asia':400, 'Africa':500}
+    #oauth
+    orequest = req.get("originalRequest") # work down the tree
+    odata = orequest.get("data") # work down the tree
+    user = odata.get("user") # work down the tree
+    access_token = user.get("access_token")
 
-    speech = "The cost of shipping to " + zone + " is " + str(cost[zone]) + " euros."
+    #data
+    result = req.get("result") # work down the tree
+    parameters = result.get("parameters") # work down the tree
+    startdate = parameters.get("start-date")
+    meetingname = parameters.get("meeting-name")
+
+    payload = {
+        "start-date": startdate,
+        "end-date": startdate,
+        "meeting-name": meetingname
+    }
+
+   # POST info to join.me
+    baseurl = "https://api.join.me/v1/meetings"
+    headers = { 
+              'content-type': 'application/json',
+               'authorization': 'Bearer ' + access_token
+             }
+    result = requests.post(baseurl, data=json.dumps(payload), headers=headers)
+    data = result.json()
+    print(data)
+    res = makeWebhookResult(data)
+    return res
+
+def makeWebhookResult(data):
+
+    speech = "Appointment scheduled!"
 
     print("Response:")
     print(speech)
@@ -44,15 +82,14 @@ def makeWebhookResult(req):
     return {
         "speech": speech,
         "displayText": speech,
-        #"data": {},
-        # "contextOut": [],
-        "source": "apiai-onlinestore-shipping"
+        # "data": data,
+        "source": "heroku-bookmyconference"
     }
 
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
 
-    print "Starting app on port %d" % port
+    print("Starting app on port %d" % port)
 
-    app.run(debug=True, port=port, host='0.0.0.0')
+    app.run(debug=False, port=port, host='0.0.0.0')
